ec7ab2f86fec5d60d3304036bdbc2be63c5f233f
ravi <ravibadami191@gmail.com>
2023-11-29 12:15:05 +0530

    login header

--- NetFlixGPT/src/components/Header.jsx
+++ NetFlixGPT/src/components/Header.jsx
@@ -1,5 +1,13 @@
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className=" absolute  px-9 py-5 w-full bg-gradient-to-b from-black  ">
+      <img
+        className=" h-14"
+        src="https://www.freepnglogos.com/uploads/netflix-logo-0.png"
+        alt=""
+      />
+    </div>
+  );
 };
 
 export default Header;
--- NetFlixGPT/src/components/Login.jsx
+++ NetFlixGPT/src/components/Login.jsx
@@ -1,14 +1,14 @@
+import Header from "./Header";
+
 const Login = () => {
   return (
     <div className="">
-      {/* <img
-        // src="https://assets.nflxext.com/ffe/siteui/vlv3/d1532433-07b1-4e39-a920-0f08b81a489e/67033404-2df8-42e0-a5a0-4c8288b4da2c/IN-en-20231120-popsignuptwoweeks-perspective_alpha_website_medium.jpg"
-        alt="hi"
-      /> */}
-
-      <div className="font-bold border-2 border-black w-4 h-4 bg-red-900 ">
-        login
-      </div>
+      <Header />
+      <img
+        className=" w-screen h-screen"
+        src="https://assets.nflxext.com/ffe/siteui/vlv3/d1532433-07b1-4e39-a920-0f08b81a489e/67033404-2df8-42e0-a5a0-4c8288b4da2c/IN-en-20231120-popsignuptwoweeks-perspective_alpha_website_medium.jpg"
+        alt=""
+      />
     </div>
   );
 };
