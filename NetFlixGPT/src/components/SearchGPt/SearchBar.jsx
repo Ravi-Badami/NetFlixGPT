981fd41286b5cfb9218fa548509dcefe81db7230
ravi <ravibadami191@gmail.com>
2023-12-09 18:55:06 +0530

    created Hook of GPT movies

--- NetFlixGPT/src/components/SearchGPt/SearchBar.jsx
+++ NetFlixGPT/src/components/SearchGPt/SearchBar.jsx
@@ -4,55 +4,15 @@ import { useRef } from "react";
 import openai from "../../utils/openAI";
 import { API_OPTIONS } from "../../utils/constants";
 import { addMovieDetails } from "../../utils/redux/gptSlice";
+import useGptMovie from "../../hooks/useGptMovie";
 
 const SearchBar = () => {
-  const dispatch = useDispatch();
   const lang = useSelector((store) => store.config.language);
   const result = useRef(null);
+  const { handleSearchOpenAi } = useGptMovie();
 
-  const searchGptMovies = async (movie) => {
-    const data = await fetch(
-      "https://api.themoviedb.org/3/search/movie?query=" +
-        movie +
-        "&include_adult=false&language=en-US&page=1",
-      API_OPTIONS
-    );
-    const json = await data.json();
-    return json.results;
-  };
-
-  const handleSearchOpenAi = async () => {
-    const gptQuery =
-      "Act as a movie recommendation system and suggest some movies for the query" +
-      result.current.value +
-      "only names of 5 movies , comma separated like the example result given .Example Result:Gadar,Sholay,Don,Golmal,Koi Mil Gaya";
-    console.log(result?.current.value);
-    try {
-      const gptResults = await openai.chat.completions.create({
-        messages: [{ role: "user", content: gptQuery }],
-        model: "gpt-3.5-turbo",
-      });
-
-      // console.log(gptResults?.choices);
-
-      console.log(gptResults?.choices[0].message.content);
-      const getMovies = gptResults?.choices[0].message.content.split(",");
-
-      /** It will return the promise because  Instead of blocking the execution of your program and waiting for the asynchronous task to complete, searchGptMovies returns a promise immediately. This promise is in a pending state until the asynchronous operation is finished.*/
-      const moviePromise = getMovies.map((m) => searchGptMovies(m));
-
-      /** The above promise is completed here */
-      const finalResult = await Promise.all(moviePromise);
-      console.log(finalResult);
-      dispatch(
-        addMovieDetails({
-          movieNames: gptResults,
-          moviesResults: finalResult,
-        })
-      );
-    } catch (error) {
-      console.log(error);
-    }
+  const handleSearchOpenAiThis = () => {
+    handleSearchOpenAi(result);
   };
 
   return (
@@ -66,7 +26,7 @@ const SearchBar = () => {
         />
         <button
           className="ml-2  py-1 px-4 font-bold bg-red-700 text-white rounded-md h-12"
-          onClick={handleSearchOpenAi}
+          onClick={handleSearchOpenAiThis}
         >
           {language[lang].search}
         </button>
