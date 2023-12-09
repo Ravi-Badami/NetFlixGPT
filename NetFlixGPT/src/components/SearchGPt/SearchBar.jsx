b4717fa5d8f08be6852538fdfe757ec6f5aebba6
ravi <ravibadami191@gmail.com>
2023-12-09 14:12:57 +0530

    fetching movies from TMDB using GPT Api completed

--- NetFlixGPT/src/components/SearchGPt/SearchBar.jsx
+++ NetFlixGPT/src/components/SearchGPt/SearchBar.jsx
@@ -2,10 +2,22 @@ import { useSelector } from "react-redux";
 import language from "../../utils/language";
 import { useRef } from "react";
 import openai from "../../utils/openAI";
+import { API_OPTIONS } from "../../utils/constants";
 
 const SearchBar = () => {
   const lang = useSelector((store) => store.config.language);
   const result = useRef(null);
+
+  const searchGptMovies = async (movie) => {
+    const data = await fetch(
+      "https://api.themoviedb.org/3/search/movie?query=" +
+        movie +
+        "&include_adult=false&language=en-US&page=1",
+      API_OPTIONS
+    );
+    const json = await data.json();
+    return json.results;
+  };
 
   const handleSearchOpenAi = async () => {
     const gptQuery =
@@ -18,9 +30,18 @@ const SearchBar = () => {
         messages: [{ role: "user", content: gptQuery }],
         model: "gpt-3.5-turbo",
       });
+
       // console.log(gptResults?.choices);
 
-      // console.log(gptResults?.choices[0].message.content);
+      console.log(gptResults?.choices[0].message.content);
+      const getMovies = gptResults?.choices[0].message.content.split(",");
+
+      /** It will return the promise because  Instead of blocking the execution of your program and waiting for the asynchronous task to complete, searchGptMovies returns a promise immediately. This promise is in a pending state until the asynchronous operation is finished.*/
+      const moviePromise = getMovies.map((m) => searchGptMovies(m));
+
+      /** The above promise is completed here */
+      const finalResult = await Promise.all(moviePromise);
+      console.log(finalResult);
     } catch (error) {
       console.log(error);
     }
