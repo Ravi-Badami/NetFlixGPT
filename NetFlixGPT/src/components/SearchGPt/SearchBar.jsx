cfe38aa27ab6ae23bff1d341c0afc15941ce78a5
ravi <ravibadami191@gmail.com>
2023-12-09 13:27:45 +0530

    GPT Api integrated

--- NetFlixGPT/src/components/SearchGPt/SearchBar.jsx
+++ NetFlixGPT/src/components/SearchGPt/SearchBar.jsx
@@ -1,17 +1,44 @@
 import { useSelector } from "react-redux";
 import language from "../../utils/language";
+import { useRef } from "react";
+import openai from "../../utils/openAI";
 
 const SearchBar = () => {
   const lang = useSelector((store) => store.config.language);
+  const result = useRef(null);
+
+  const handleSearchOpenAi = async () => {
+    const gptQuery =
+      "Act as a movie recommendation system and suggest some movies for the query" +
+      result.current.value +
+      "only names of 5 movies , comma separated like the example result given .Example Result:Gadar,Sholay,Don,Golmal,Koi Mil Gaya";
+    console.log(result?.current.value);
+    try {
+      const gptResults = await openai.chat.completions.create({
+        messages: [{ role: "user", content: gptQuery }],
+        model: "gpt-3.5-turbo",
+      });
+      // console.log(gptResults?.choices);
+
+      // console.log(gptResults?.choices[0].message.content);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className=" mt-24">
       <form action="" onSubmit={(e) => e.preventDefault()}>
         <input
+          ref={result}
           type="text"
           className=" px-5 pr-32 py-1 rounded-md h-12 outline-none "
           placeholder={language[lang].placeHolder}
         />
-        <button className="ml-2  py-1 px-4 font-bold bg-red-700 text-white rounded-md h-12">
+        <button
+          className="ml-2  py-1 px-4 font-bold bg-red-700 text-white rounded-md h-12"
+          onClick={handleSearchOpenAi}
+        >
           {language[lang].search}
         </button>
       </form>
--- NetFlixGPT/src/utils/constants.js
+++ NetFlixGPT/src/utils/constants.js
@@ -1,3 +1,5 @@
+import { OPENAI_KEY } from "../../src/env";
+
 export const LOGO = "https://www.freepnglogos.com/uploads/netflix-logo-0.png";
 export const BG_IMAGE =
   "https://assets.nflxext.com/ffe/siteui/vlv3/d1532433-07b1-4e39-a920-0f08b81a489e/67033404-2df8-42e0-a5a0-4c8288b4da2c/IN-en-20231120-popsignuptwoweeks-perspective_alpha_website_medium.jpg";
@@ -20,3 +22,6 @@ export const SUPPORT_LANGUAGE = [
   { identifier: "hindi", name: "Hindi" },
   { identifier: "spanish", name: "Spanish" },
 ];
+
+const OPENAI_GPT_API = OPENAI_KEY;
+console.log(OPENAI_GPT_API);
