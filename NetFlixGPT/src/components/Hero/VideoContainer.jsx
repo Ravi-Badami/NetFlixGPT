67f73b3c159e6ec321ec3c76608365469e2000ef
Ravi-Badami <ravibadami191@gmail.com>
2024-01-21 23:19:12 +0530

    .

--- NetFlixGPT/src/components/Hero/VideoContainer.jsx
+++ NetFlixGPT/src/components/Hero/VideoContainer.jsx
@@ -12,16 +12,11 @@ const VideoContainer = ({ movieId }) => {
 
   return (
     <div className=' w-screen  -z-10 border border-white '>
-      {/* <iframe
-        className="w-[100%] h-screen  "
-        src={
-          "https://www.youtube.com/embed/" +
-          result +
-          "?autoplay=1&controls=0&mute=1"
-        }
-        title="YouTube video player"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-      ></iframe> */}
+      <iframe
+        className='w-[100%] h-screen  '
+        src={'https://www.youtube.com/embed/' + result + '?autoplay=1&controls=0&mute=1'}
+        title='YouTube video player'
+        allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'></iframe>
     </div>
   );
 };
